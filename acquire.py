2ea68f5d03f9d4910b01d9ef8d057c5556789020
bradgauvin <bradley.gauvin@gmail.com>
2022-06-29 15:39:01 -0700

    pulled out jupyter notebook data

--- acquire.py
+++ acquire.py
@@ -1,36 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# Make a new python module, `acquire.py` to hold the following data aquisition functions:
-# 
-# 7. Make a function named `get_titanic_data` that returns the titanic data from the codeup data science database as a pandas data frame. Obtain your data from the _Codeup Data Science Database_. 
-# 
-# 
-# 8. Make a function named `get_iris_data` that returns the data from the `iris_db` on the codeup data science database as a pandas data frame. The returned data frame should include the actual name of the species in addition to the `species_id`s. Obtain your data from the _Codeup Data Science Database_. 
-# 
-# 9. Make a function named `get_telco_data` that returns the data from the `telco_churn` database in SQL. In your SQL, be sure to join all 4 tables together, so that the resulting dataframe contains all the contract, payment, and internet service options. Obtain your data from the _Codeup Data Science Database_. 
-# 
-# 10. Once you've got your `get_titanic_data`, `get_iris_data`, and `get_telco_data` functions written, now it's time to add caching to them. To do this, edit the beginning of the function to check for the local filename of `telco.csv`, `titanic.csv`, or `iris.csv`. If they exist, use the .csv file. If the file doesn't exist, then produce the SQL and pandas necessary to create a dataframe, then write the dataframe to a .csv file with the appropriate name. 
-# 
-# __Be sure to add env.py, titanic.csv, iris.csv, and telco.csv to your .gitignore file__
-
-# In[7]:
-
-
 import pandas as pd
 import numpy as np
 import os
 from env import host, user, password
 
 
-# In[8]:
-
-
 def get_connection(db, user=user, host=host,password=password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
-
-
-# In[11]:
 
 
 # New Titanic Data Set
@@ -38,10 +13,6 @@ def new_titanic_data():
     sql_query = 'SELECT * FROM passengers'
     df = pd.read_sql(sql_query, get_connection('titanic_db'))
     return df
-
-
-# In[13]:
-
 
 # Acquire Titnaic Data 
 def get_titanic_data():
@@ -65,9 +36,6 @@ def get_titanic_data():
         return df  
 
 
-# In[15]:
-
-
 ### Iris Data
 
 def new_iris_data():
@@ -84,9 +52,6 @@ def new_iris_data():
               '''
     df = pd.read_sql(sql_query, get_connection('iris_db'))
     return df
-
-
-# In[16]:
 
 
 def get_iris_data():
@@ -107,9 +72,6 @@ def get_iris_data():
         return df 
 
 
-# In[17]:
-
-
 ### Get Telco Data
 def new_telco_data():
     sql_query = '''
@@ -120,10 +82,6 @@ def new_telco_data():
             '''
     df=pd.read_sql(sql_query, get_connection('telco_churn'))
     return df
-
-
-# In[18]:
-
 
 def get_telco_data():
     
